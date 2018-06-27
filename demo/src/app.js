f5db9e807fd2bca6cb5e82c981c78b9de465a130
Dizzle <developerdizzle@gmail.com>
2018-06-26 22:44:08 -0400

    Refactors into children-as-function

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,32 +1,130 @@
-import React from 'react';
+import React, { PureComponent, PropTypes } from 'react';
 import { render } from 'react-dom';
-import ConfigurableExample from './ConfigurableExample';
+import VirtualList from '../../src/VirtualList';
 
 require('file?name=[name].[ext]!./index.html');
 
-const MyList = ({
-  virtual,
-  itemHeight,
-}) => (
-  <ul className="media-list list-group" style={virtual.style}>
-    {virtual.items.map((item) => (
-      <li key={`item${item.id}`} className="list-group-item" style={{height: itemHeight }}>
-        <div className="media-left">
-          <img className="media-object" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjEzLjQ2ODc1IiB5PSIzMiIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMHB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjY0eDY0PC90ZXh0PjwvZz48L3N2Zz4=" />
-        </div>
-        <div className="media-body">
-          <h4 className="media-heading">{item.title}</h4>
-          <p>{item.text}</p>
-        </div>
-      </li>
-    ))}
-  </ul>
-);
+const makeItem = (i) => ({
+  id: i,
+  title: `Media heading #${i+1}`,
+  text: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.',
+});
 
-const MyConfigurableExample = ConfigurableExample(MyList);
+class App extends PureComponent {
+  constructor() {
+    super();
+
+    const defaultItemCount = 100000;
+
+    const items = [];
+
+    for (let i = 0; i < defaultItemCount; i++) {
+      items[i] = makeItem(i);
+    }
+    
+    const state = {
+      itemHeight: 100,
+      itemCount: defaultItemCount,
+      items: items,
+      contained: false,
+      containerHeight: 250,
+      itemBuffer: 0,
+    };
+    
+    this.state = state;
+  };
+
+  update = () => {
+    const items = [];
+    const itemCount = parseInt(this.refs.itemCount.value, 10);
+
+    for (var i = 0; i < itemCount; i++) {
+      items[i] = makeItem(i);
+    }
+    
+    const contained = this.refs.contained.checked;
+    
+    const state = {
+      itemHeight: parseInt(this.refs.itemHeight.value, 10),
+      itemCount: itemCount,
+      items: items,
+      contained: contained,
+      container: this.refs.container,
+      containerHeight: parseInt(this.refs.containerHeight.value, 10),
+      itemBuffer: parseInt(this.refs.itemBuffer.value, 10),
+    };
+
+    this.setState(state);
+  };
+
+  renderItems = (virtual) => {
+    return (
+      <ul className="media-list list-group" style={virtual.style}>
+        {virtual.items.map((item) => (
+          <li key={item.id} className="list-group-item" style={{height: this.state.itemHeight }}>
+            <div className="media-left">
+              <img className="media-object" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjEzLjQ2ODc1IiB5PSIzMiIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMHB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjY0eDY0PC90ZXh0PjwvZz48L3N2Zz4=" />
+            </div>
+            <div className="media-body">
+              <h4 className="media-heading">{item.title}</h4>
+              <p>{item.text}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <div role="form" className="form-horizontal">
+          <div className="form-group">
+            <label className="col-xs-6 col-sm-2" htmlFor="contained">Contained</label>
+            <div className="col-xs-6 col-sm-4">
+              <input onChange={this.update} className="form-control" type="checkbox" checked ={this.state.contained} id="contained" ref="contained" />
+            </div>
+            <label className="col-xs-6 col-sm-2" htmlFor="containerHeight">Container Height</label>
+            <div className="col-xs-6 col-sm-4">
+              <input onChange={this.update} className="form-control" type="number" min="0" max="10000" value={this.state.containerHeight} id="containerHeight" ref="containerHeight" />
+            </div>
+          </div>
+          <div className="form-group">
+            <label className="col-xs-6 col-sm-2" htmlFor="itemHeight">Item Height</label>
+            <div className="col-xs-6 col-sm-4">
+              <input onChange={this.update} className="form-control" type="number" min="0" value={this.state.itemHeight} id="itemHeight" ref="itemHeight" />
+            </div>
+            <label className="col-xs-6 col-sm-2" htmlFor="itemCount">Item Count</label>
+            <div className="col-xs-6 col-sm-4">
+              <input onChange={this.update} className="form-control" type="number" min="0" value={this.state.itemCount} id="itemCount" ref="itemCount" />
+            </div>
+          </div>
+          <div className="form-group">
+            <label className="col-xs-6 col-sm-2" htmlFor="scrollDelay">Item Buffer</label>
+            <div className="col-xs-6 col-sm-4">
+              <input onChange={this.update} className="form-control" type="number" min="0" value={this.state.itemBuffer} id="itemBuffer" ref="itemBuffer" />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12" id="container" ref="container" style={this.state.contained ? { overflow: 'scroll', height: this.state.containerHeight } : {}}>
+            <VirtualList
+              items={this.state.items}
+              itemBuffer={this.state.itemBuffer}
+              itemHeight={this.state.itemHeight}
+              container={this.state.contained ? this.state.container : window}
+              >
+              {this.renderItems}
+            </VirtualList>
+          </div>
+        </div>
+      </div>
+    );
+  };
+};
 
 render(
-  <MyConfigurableExample />,
+  <App />,
   document.getElementById('app')
 );
 
--- src/VirtualList.js
+++ src/VirtualList.js
@@ -4,109 +4,102 @@ import PropTypes from 'prop-types';
 
 import getVisibleItemBounds from './utils/getVisibleItemBounds';
 import throttleWithRAF from './utils/throttleWithRAF';
-import defaultMapToVirtualProps from './utils/defaultMapVirtualToProps';
+import defaultMapVirtualParameter from './utils/defaultMapVirtualParameter';
 
-const VirtualList = (options, mapVirtualToProps = defaultMapToVirtualProps) => (InnerComponent) => {
-  return class vlist extends PureComponent {
-    static propTypes = {
-      items: PropTypes.array.isRequired,
-      itemHeight: PropTypes.number.isRequired,
-      itemBuffer: PropTypes.number,
-    };
-
-    static defaultProps = {
-      itemBuffer: 0,
-    };
-
-    _isMounted = false;
-
-    constructor(props) {
-      super(props);
-
-      this.options = {
-        container: typeof window !== 'undefined' ? window : undefined,
-        ...options,
-      };
-
-      this.state = {
-        firstItemIndex: 0,
-        lastItemIndex: -1,
-      };
-
-      // initialState allows us to set the first/lastItemIndex (useful for server-rendering)
-      if (options && options.initialState) {
-        this.state = {
-          ...this.state,
-          ...options.initialState,
-        };
-      }
-
-      this.refreshState = this.refreshState.bind(this);
-
-      // if requestAnimationFrame is available, use it to throttle refreshState
-      if (typeof window !== 'undefined' && 'requestAnimationFrame' in window) {
-        this.refreshState = throttleWithRAF(this.refreshState);
-      }
-    };
-
-    setStateIfNeeded(list, container, items, itemHeight, itemBuffer) {
-      // get first and lastItemIndex
-      const state = getVisibleItemBounds(list, container, items, itemHeight, itemBuffer);
-
-      if (state === undefined) { return; }
-      
-      if (state.firstItemIndex > state.lastItemIndex) { return; }
-
-      if (state.firstItemIndex !== this.state.firstItemIndex || state.lastItemIndex !== this.state.lastItemIndex) {
-        this.setState(state);
-      }
-    }
-
-    refreshState() {
-      if (!this._isMounted) {
-        return;
-      }
-      
-      const { itemHeight, items, itemBuffer } = this.props;
-
-      this.setStateIfNeeded(this.domNode, this.options.container, items, itemHeight, itemBuffer);
-    };
-
-    componentWillMount() {
-      this._isMounted = true;
-    }
-
-    componentDidMount() {
-      // cache the DOM node
-      this.domNode = ReactDOM.findDOMNode(this);
-
-      // we need to refreshState because we didn't have access to the DOM node before
-      this.refreshState();
-
-      // add events
-      this.options.container.addEventListener('scroll', this.refreshState);
-      this.options.container.addEventListener('resize', this.refreshState);
-    };
-
-    componentWillUnmount() {
-      this._isMounted = false;
-
-      // remove events
-      this.options.container.removeEventListener('scroll', this.refreshState);
-      this.options.container.removeEventListener('resize', this.refreshState);
-    };
-
-    // if props change, just assume we have to recalculate
-    componentWillReceiveProps(nextProps) {
-      const { itemHeight, items, itemBuffer } = nextProps;
-
-      this.setStateIfNeeded(this.domNode, this.options.container, items, itemHeight, itemBuffer);
-    };
-
-    render() {
-      return (<InnerComponent {...this.props} {...mapVirtualToProps(this.props, this.state)} />);
-    };
+class VirtualList extends PureComponent {
+  static propTypes = {
+    items: PropTypes.array.isRequired,
+    itemHeight: PropTypes.number.isRequired,
+    itemBuffer: PropTypes.number.isRequired,
+    container: PropTypes.object.isRequired,
+    mapVirtualToProps: PropTypes.func.isRequired,
+    initialState: PropTypes.object.isRequired,
+    children: PropTypes.func.isRequired,
   };
-};
+
+  static defaultProps = {
+    itemBuffer: 0,
+    container: typeof window !== 'undefined' ? window : undefined,
+    mapVirtualToProps: defaultMapVirtualParameter,
+    initialState: {
+      firstItemIndex: 0,
+      lastItemIndex: -1,
+    },
+  };
+
+  _isMounted = false;
+
+  constructor(props) {
+    super(props);
+
+    // initialState allows us to set the first/lastItemIndex (useful for server-rendering)
+    this.state = props.initialState;
+
+    // if requestAnimationFrame is available, use it to throttle refreshState
+    if (typeof window !== 'undefined' && 'requestAnimationFrame' in window) {
+      this.refreshState = throttleWithRAF(this.refreshState);
+    }
+  };
+
+  setStateIfNeeded(list, container, items, itemHeight, itemBuffer) {
+    // get first and lastItemIndex
+    const state = getVisibleItemBounds(list, container, items, itemHeight, itemBuffer);
+
+    if (state === undefined) { return; }
+    
+    if (state.firstItemIndex > state.lastItemIndex) { return; }
+
+    if (state.firstItemIndex !== this.state.firstItemIndex || state.lastItemIndex !== this.state.lastItemIndex) {
+      this.setState(state);
+    }
+  }
+
+  refreshState = () => {
+    if (!this._isMounted) {
+      return;
+    }
+    
+    const { itemHeight, items, itemBuffer, container } = this.props;
+
+    this.setStateIfNeeded(this.domNode, container, items, itemHeight, itemBuffer);
+  };
+
+  componentWillMount() {
+    this._isMounted = true;
+  }
+
+  componentDidMount() {
+    // cache the DOM node
+    this.domNode = ReactDOM.findDOMNode(this);
+
+    // we need to refreshState because we didn't have access to the DOM node before
+    this.refreshState();
+
+    // add events
+    this.props.container.addEventListener('scroll', this.refreshState);
+    this.props.container.addEventListener('resize', this.refreshState);
+  };
+
+  componentWillUnmount() {
+    this._isMounted = false;
+
+    // remove events
+    this.props.container.removeEventListener('scroll', this.refreshState);
+    this.props.container.removeEventListener('resize', this.refreshState);
+  };
+
+  // if props change, just assume we have to recalculate
+  componentWillReceiveProps(nextProps) {
+    const { itemHeight, items, itemBuffer, container } = nextProps;
+
+    this.setStateIfNeeded(this.domNode, container, items, itemHeight, itemBuffer);
+  };
+
+  render() {
+    const virtual = this.props.mapVirtualToProps(this.props, this.state);
+
+    return this.props.children(virtual);
+  };
+}
 
 export default VirtualList;
